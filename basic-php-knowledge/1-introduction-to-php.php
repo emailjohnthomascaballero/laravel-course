459424218557822d69ab85178e3b8bc666c55a42
John <emailjohnthomascaballero@gmail.com>
2024-01-16 09:00:55 +0800

    Day 1

---
+++ basic-php-knowledge/1-introduction-to-php.php
@@ -0,0 +1,4 @@
+<!-- - Lesson 1: Introduction to PHP
+- Step 1: Understand what PHP is and its role in web development.
+- Exercise: Write a simple "Hello, World!" script in PHP and execute it. -->
+

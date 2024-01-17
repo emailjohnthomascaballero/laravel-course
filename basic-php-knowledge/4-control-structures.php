3550d4ffc8645e1c1409887919d9847f92af7dba
John <emailjohnthomascaballero@gmail.com>
2024-01-17 10:55:16 +0800

    Day 2

---
+++ basic-php-knowledge/4-control-structures.php
@@ -0,0 +1,19 @@
+<!-- Lesson 5: Control Structures**
+- Step 5: Study conditional statements (if, else, switch) and loops (for, while, foreach) in PHP.
+- Exercise: Write a program that uses conditionals and loops to solve a simple problem. -->
+
+<!-- Conditional Statements:
+
+if-else: Executes one block of code if a condition is true, and another block if it's false. -->
+
+<?php
+
+$grade = 80;
+
+if ($grade >= 75) {
+  echo 'You passed the exam';
+} else {
+  echo 'You fail, better luck next time';
+}
+
+?>

1df82c3241eed063f97a11e0784cd221686945d2
John <emailjohnthomascaballero@gmail.com>
2024-01-28 21:21:19 +0800

    Day 13

--- 30-days-to-php-mastery/calculator-project/index.php
+++ 30-days-to-php-mastery/calculator-project/index.php
@@ -1,3 +1,5 @@
+<!-- SOURCE: https://youtu.be/vjw5UmY7g9M?si=xDuzTm7nxohToc5N -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +9,30 @@
 </head>
 <body>
   
+  <!-- htmlspecialchars() - used to converts special characters to will sanitize users input to prevent XSS Cross Site Scripting. -->
+  <!-- $_SERVER["PHP_SELF"] - send the data to the same page. -->
+  <!-- post - we don`t see the data inside the URL.-->
+
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+    <input type="number" name="num01" placeholder="Number one">
+    <select name="operator" id="">
+      <option value="add">+</option>
+      <option value="subtract">-</option>
+      <option value="multiply">*</option>
+      <option value="divide">/</option>
+    </select>
+    <input type="number" name="num02" placeholder="Number two">
+    <button>Calculate</button>
+  </form>
+
+  <?php
   
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $num01 = filter_input(INPUT_POST, "num01", FILTER_SANITIZE_NUMBER_FLOAT);
+    $num02 = filter_input(INPUT_POST, "num02", FILTER_SANITIZE_NUMBER_FLOAT);
+  }
+
+  ?>
 
 </body>
 </html>

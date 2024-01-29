9f43f8578090aac6d4499719af4dfda60b40ab9b
John <emailjohnthomascaballero@gmail.com>
2024-01-29 11:50:03 +0800

    Day 14

--- 30-days-to-php-mastery/calculator-project/index.php
+++ 30-days-to-php-mastery/calculator-project/index.php
@@ -30,8 +30,8 @@
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $num01 = filter_input(INPUT_POST, "num01", FILTER_SANITIZE_NUMBER_FLOAT);
     $num02 = filter_input(INPUT_POST, "num02", FILTER_SANITIZE_NUMBER_FLOAT);
+    $operator = htmlspecialchars($_POST["operator"]);
   }
-
   ?>
 
 </body>
